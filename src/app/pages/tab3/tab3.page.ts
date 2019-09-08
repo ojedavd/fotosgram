b65a3668edd946fd55366e8f87e749b79114b743
Victor <ojedavd@gmail.com>
2019-09-08 16:44:19 -0300

    Actualizar el usuario en la BD

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../../interfaces/interfaces';
+import { NgForm } from '@angular/forms';
+import { UiServiceService } from '../../services/ui-service.service';
 
 @Component({
   selector: 'app-tab3',
@@ -11,11 +13,26 @@ export class Tab3Page implements OnInit {
 
   usuario: Usuario = {};
 
-  constructor( private usuarioService: UsuarioService) {}
+  constructor( private usuarioService: UsuarioService,
+               private uiService: UiServiceService) {}
 
   ngOnInit() {
 
     this.usuario = this.usuarioService.getUsuario();
+
+  }
+
+  async actualizar( fActualizar: NgForm ) {
+
+    if ( fActualizar.invalid ) { return; }
+
+    const actualizado = await this.usuarioService.actualizarUsuario( this.usuario );
+    console.log(actualizado);
+    if ( actualizado ) {
+      this.uiService.presentToast('Registro actualizado');
+    } else {
+      this.uiService.presentToast('No se pudo actualizar');
+    }
 
   }
 
