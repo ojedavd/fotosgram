441b52aa462eba9d5a1aefb94219913aa5f78332
Victor <ojedavd@gmail.com>
2019-09-08 11:09:33 -0300

    Componente para seleccionar un avatar

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -14,45 +14,6 @@ export class LoginPage implements OnInit {
 
   @ViewChild('slidePrincipal', {static: true}) slides: IonSlides;
 
-  avatars = [
-    {
-      img: 'av-1.png',
-      seleccionado: true
-    },
-    {
-      img: 'av-2.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-3.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-4.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-5.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-6.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-7.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-8.png',
-      seleccionado: false
-    },
-  ];
-
-  avatarSlide = {
-    slidesPerView: 3.5
-  };
-
   loginUser = {
     email: 'ojedavd@gmail.com',
     password: '123'
@@ -99,13 +60,6 @@ export class LoginPage implements OnInit {
       this.uiService.alertaInformativa('Ya existe el usuario.');
     }
 
-  }
-
-  seleccionarAvatar( avatar ) {
-
-    this.avatars.forEach( av => av.seleccionado = false );
-
-    avatar.seleccionado = true;
   }
 
   mostrarRegistro() {
